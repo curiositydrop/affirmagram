b85907543c7d1a08244d532fc5426e1d454ccffa
Curiositydrop <info@curiositydrop.com>
2025-08-11 15:32:13 -0400

    Create firebase-config.js

---
+++ Demo-site/js/firebase-config.js
@@ -0,0 +1,8 @@
+window.FB_CONFIG = {
+  apiKey: "AIzaSyD3hHhNFCizXJMPF1JUykV3aZPQ6dXcSFI",
+  authDomain: "venomous-thorn-wall.firebaseapp.com",
+  projectId: "venomous-thorn-wall",
+  storageBucket: "venomous-thorn-wall.appspot.com",
+  messagingSenderId: "673303332710",
+  appId: "1:673303332710:web:9090614971aab7b4dc5b8d"
+};
