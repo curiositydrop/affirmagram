e69997c5215d6483365def1d0ca4ba2f286f39db
Curiositydrop <info@curiositydrop.com>
2025-08-11 22:47:54 -0400

    Update firebase-config.js

--- Demo-site/js/firebase-config.js
+++ Demo-site/js/firebase-config.js
@@ -1,9 +1,10 @@
-// firebase-config.js
-// This just holds your Firebase keys in a global variable.
+// js/firebase-config.js
 
+// Your Firebase configuration
 window.FB_CONFIG = {
   apiKey: "AIzaSyBAkwOyKJ2Y6_LtXZKUp5CauPvxC0dJr6A",
   authDomain: "bandfanwall.firebaseapp.com",
+  databaseURL: "https://bandfanwall-default-rtdb.firebaseio.com", // <-- Added for Realtime DB
   projectId: "bandfanwall",
   storageBucket: "bandfanwall.firebasestorage.app",
   messagingSenderId: "619241154826",
